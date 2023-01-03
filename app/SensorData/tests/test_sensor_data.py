c954299a7f24edc958a3a2060c7bb62d514b1dcd
big-hope <79042347+big-hope@users.noreply.github.com>
2023-01-03 15:08:56 +0200

    added b64 encoding for tests

--- app/SensorData/tests/test_sensor_data.py
+++ app/SensorData/tests/test_sensor_data.py
@@ -1,6 +1,8 @@
 """
 Tests for SensorData APIs.
 """
+import base64
+import json
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -26,6 +28,13 @@ EXAMPLE_DATA = {
     },
     "subscription": "projects/myproject/subscriptions/mysubscription"
 }
+
+
+def dict2b64(payload: dict) -> str:
+    "Convert dict to base64"
+    payload_bytes = json.dumps(payload).encode('utf-8')
+    payload_b64_bytes = base64.b64encode(payload_bytes)
+    return payload_b64_bytes.decode('utf-8')
 
 
 def create_device():
@@ -58,33 +67,29 @@ class SensorDataAPITests(TestCase):
         """Test post data."""
         device = create_device()
 
-        # payload = {
-        #     "serial": device.serial_number,
-        #     "v0": "100013",
-        #     "v18": "2.72",
-        #     "Time": "2022-11-08 4:00:04"
-        # }
-        EXAMPLE_DATA["message"]["data"] = "ewoJInNlcmlhbCI6ICIxMDAwMDA \
-        xMDAiLAoJInYwIjogMTAwMDEzLAoJInYxO \
-        CI6IDIuNzIsCgkiVGltZSI6ICIyMDIyLTExLTA4IDQ6MDA6MDQiCn0="
+        payload = {"serial": device.serial_number,
+                   "v0": "100013",
+                   "v18": "2.72",
+                   "Time": "2022-11-08 4:00:04"
+                   }
 
+        payload_b64 = dict2b64(payload)
+        EXAMPLE_DATA["message"]["data"] = payload_b64
         res = self.client.post(SENSOR_DATA_URL, EXAMPLE_DATA, format='json')
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
     def test_create_data_with_not_exist_serial_number(self):
         """Test creating data with not-existing S/N."""
 
-        # not_existsing_sn = '00000001'
-
-        # payload = {
-        #     "serial": not_existsing_sn,
-        #     "v0": "100013",
-        #     "v18": "2.72",
-        #     "Time": "2022-11-08 4:00:04"
-        # }
-        EXAMPLE_DATA["message"]["data"] = "ewoJInNlcmlhbCI6ICIyMDAwMDAxMDA \
-        iLAoJInYwIjogMTAwMDEzLAoJInYxOCI6IDIuNzIsCgkiVGltZSI6ICIyMDIyLTE \
-        xLTA4IDQ6MDA6MDQiCn0="
+        not_existsing_sn = '00000001'
+        payload = {
+            "serial": not_existsing_sn,
+            "v0": "100013",
+            "v18": "2.72",
+            "Time": "2022-11-08 4:00:04"
+        }
+        payload_b64 = dict2b64(payload)
+        EXAMPLE_DATA["message"]["data"] = payload_b64
 
         res = self.client.post(SENSOR_DATA_URL, EXAMPLE_DATA, format='json')
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
